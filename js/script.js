61e67f7f1ecca6c0f3677e800cc4e1e4601b67f5
namn310 <namn3102003@gmail.com>
2024-05-16 14:08:10 +0700

    16/5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,56 @@
 $(document).ready(function(){
+    
     $(".button-list-phone button").click(function(){
         $(".menu-phone").toggle();  
     })
     
-    // function tìm kiếm sản phẩm
+    /* function tìm kiếm sản phẩm
     $(".product-list").filter("#name-product");
     $("#buy").click(function mua(){
         $name_sp=$("#name-product").text().toLowerCase();
         console.log($name_sp);
     })
+    */
+
+    //thêm sản phẩm vào giỏ
+    $("#buy").click(function(){
+        $(".giohang").show();
+    })
+    // function thanh toans
+    $("#thanhtoan").click(function(){
+        $("#pay-form").toggle();
+    })
+    document.getElementById("payment").onclick=function(){
+        const listPayment=document.getElementsByName("payment");
+        for (var i = 0; i < listPayment.length; i++){
+            if (listPayment[i].checked === true && listPayment[i].value.indexOf('payment1') > -1 ){
+                $(".payment1-detail").show();
+            }
+        }
+    }
+
     
 })
+
+/*
+document.addEventListener
+document.getElementById("id1").onclick=function(){
+    const listPayment=document.getElementsByName("payment");
+    for (var i = 0; i < listPayment.length; i++){
+        if (listPayment[i].checked === true){
+            alert(listPayment[i].value);
+        }
+    }
+}
+   
+    
+
+  
+   let ranks = ['A', 'B', 'C'];
+ 
+ranks.forEach(function (e) {
+    console.log(e); // A B C
+});
+    
+
+    
---
+++ js/script2.js
@@ -0,0 +1,6 @@
+ const numbers=[1,2,3,4,5];
+    const sum=0;
+    foreach(numbers, funtion(element){
+        sum+=element;
+    });
+    console.log(sum);
